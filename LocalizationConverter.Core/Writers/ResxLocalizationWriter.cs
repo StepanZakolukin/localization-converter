3850cf521cd8b1c2e02ecf2c97838ec2a1c25f48
stepan <stepan.zakolyukin@yandex.ru>
2026-07-14 17:42:08 +0500

    Рефакторинг

--- LocalizationConverter.Core/Writers/ResxLocalizationWriter.cs
+++ LocalizationConverter.Core/Writers/ResxLocalizationWriter.cs
@@ -30,58 +30,54 @@ public class ResxLocalizationWriter(string directoryPath) : ILocalizationWriter
         }
     }
 
-    // TODO: надо рефакторить, возможно имеет смысл вынести в отдельный класс
     private void GenerateResxFile(LocalizationFile file, string targetDirectoryPath)
     {
         ArgumentNullException.ThrowIfNull(file);
         ArgumentException.ThrowIfNullOrWhiteSpace(targetDirectoryPath);
 
-        // 1. Формируем полный путь к конечному файлу ресурса
         var targetPath = Path.Combine(targetDirectoryPath, file.Name + FileExtensions.Resx);
+        var root = CreateXmlRoot();
 
-        // 2. Создаем корневой элемент со стандартными заголовками Microsoft
-        var root = new XElement(ResxConstants.Nodes.Root,
+        foreach (var localization in file.LocalizationList)
+            if (!string.IsNullOrWhiteSpace(localization.Key))
+                root.Add(CreateDataElement(localization));
+
+        var resxDoc = new XDocument(new XDeclaration("1.0", "utf-8", "yes"), root);
+        resxDoc.Save(targetPath);
+    }
+
+    private XElement CreateXmlRoot()
+    {
+        return new XElement(ResxConstants.Nodes.Root,
             new XElement(
                 ResxConstants.Nodes.Header,
                 new XAttribute(ResxConstants.Nodes.NameAttribute, ResxConstants.Keys.MimeType),
                 new XElement(ResxConstants.Nodes.Value, ResxConstants.Values.MimeType)
-                ),
+            ),
             new XElement(
                 ResxConstants.Nodes.Header,
-                new XAttribute(ResxConstants.Nodes.NameAttribute,
-                ResxConstants.Keys.Version),
+                new XAttribute(ResxConstants.Nodes.NameAttribute, ResxConstants.Keys.Version),
                 new XElement(ResxConstants.Nodes.Value, ResxConstants.Values.Version)
-                ),
+            ),
             new XElement(
                 ResxConstants.Nodes.Header,
                 new XAttribute(ResxConstants.Nodes.NameAttribute, ResxConstants.Keys.Reader),
                 new XElement(ResxConstants.Nodes.Value, ResxConstants.Values.ReaderType)
-                ),
+            ),
             new XElement(
                 ResxConstants.Nodes.Header,
-                new XAttribute(ResxConstants.Nodes.NameAttribute,
-                ResxConstants.Keys.Writer),
+                new XAttribute(ResxConstants.Nodes.NameAttribute, ResxConstants.Keys.Writer),
                 new XElement(ResxConstants.Nodes.Value, ResxConstants.Values.WriterType)
-                )
+            )
         );
+    }
 
-        // 3. Наполняем XML данными из коллекции переводов
-        foreach (var localization in file.LocalizationList)
-        {
-            if (string.IsNullOrWhiteSpace(localization.Key))
-                continue;
-
-            var dataElement = new XElement(ResxConstants.Nodes.Data,
-                new XAttribute(ResxConstants.Nodes.NameAttribute, localization.Key),
-                new XAttribute(ResxConstants.Nodes.SpaceAttribute, ResxConstants.Nodes.PreserveValue),
-                new XElement(ResxConstants.Nodes.Value, localization.Value ?? string.Empty)
-            );
-
-            root.Add(dataElement);
-        }
-
-        // 4. Записываем готовый XML-документ на диск
-        var resxDoc = new XDocument(new XDeclaration("1.0", "utf-8", "yes"), root);
-        resxDoc.Save(targetPath);
+    private XElement CreateDataElement(Localization localization)
+    {
+        return new XElement(ResxConstants.Nodes.Data,
+            new XAttribute(ResxConstants.Nodes.NameAttribute, localization.Key),
+            new XAttribute(ResxConstants.Nodes.SpaceAttribute, ResxConstants.Nodes.PreserveValue),
+            new XElement(ResxConstants.Nodes.Value, localization.Value ?? string.Empty)
+        );
     }
 }
